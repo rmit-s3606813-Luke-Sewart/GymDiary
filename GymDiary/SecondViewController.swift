5bd59da69cf69f5d2f259fba29801080b7ddea87
Luke Sewart <s3606813@w8408843.cs.rmit.edu.au>
2017-08-15 11:33:36 +1000

    Initial Commit

---
+++ GymDiary/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GymDiary
+//
+//  Created by Luke Sewart on 8/15/17.
+//  Copyright © 2017 Luke Sewart. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
