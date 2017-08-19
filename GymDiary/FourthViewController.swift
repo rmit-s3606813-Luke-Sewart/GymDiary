1f28c89904879fd4704e8cd361933fefde40effd
Lukey Sew <lukeysew@LearniOSFreecoms-Mac.local>
2017-08-19 21:07:00 +0100

    20th of August commit, just added third and fourth view controllers, early in development

---
+++ GymDiary/FourthViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FourthViewController.swift
+//  GymDiary
+//
+//  Created by Lukey Sew on 19/8/17.
+//  Copyright © 2017 Luke Sewart. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ GymDiary/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  GymDiary
+//
+//  Created by Lukey Sew on 19/8/17.
+//  Copyright © 2017 Luke Sewart. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
