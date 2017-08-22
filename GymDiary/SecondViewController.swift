67c0d78fd413f30b5835459d8d82e4e500b5c7f3
Lukey Sew <lukeysew@LearniOSFreecoms-Mac.local>
2017-08-22 01:32:42 +0100

    hhhhh

--- GymDiary/SecondViewController.swift
+++ GymDiary/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
