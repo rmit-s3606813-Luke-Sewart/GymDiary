e0e429aa233156cae4d257f7f7fa3e2a330bf6b0
Lukey Sew <lukeysew@LearniOSFreecoms-Mac.local>
2017-08-26 02:12:32 +0100

    working on collection view

--- GymDiary/SecondViewController.swift
+++ GymDiary/SecondViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
 
+    var array = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        array = ["Bicep Curl","Tricep Pulldown","Leg Press","Push Up","Lat Pulldown","Chest Press","Sit Up","Chest Fly","Shoulder Press","DeadLlift","Leg Curl","Pull Up"]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,13 +25,12 @@ class CollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return array.count
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
-        
-        
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+        let Label = cell.viewWithTag(1) as! UILabel
+        Label.text = array[indexPath.row]
         return cell
     }
 }
